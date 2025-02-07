00db5e4768b699e650ee9e6378adf05505658ea7
hdezsubastas <ehernandez@mortonsubastas.com>
2025-02-07 17:17:05 -0600

    history page

--- src/views/history-page.vue
+++ src/views/history-page.vue
@@ -1,6 +1,20 @@
 <template>
+
     <NavbarDark/>
 
+    <section class="image-cover faq-sec text-center" :style="{ background: `#eff6ff url(${bg}) no-repeat`, backgroundSize: 'cover' }" data-overlay="6">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 position-relative z-1">
+                    <h1 class="text-light">Preguntas Frecuentes</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        
+    </section>
 
     <FooterTop/>
     <FooterDark/>
@@ -12,4 +26,6 @@
     import FooterTop from '@/components/footer/footer-top.vue';
     import NavbarDark from '@/components/navbar/navbar-dark.vue';
     import scrollToTop from '@/components/scroll-to-top.vue';
+    import bg from '@/assets/img/faq-sec.jpg'
+
 </script>
